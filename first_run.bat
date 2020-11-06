8d7b2af1dac753e94ca6c6bd2a1b63b85c9741ba
SkyTing <mcskyding@vip.qq.com>
2020-11-06 11:43:15 +0800

    add auto run commond

---
+++ first_run.bat
@@ -0,0 +1,20 @@
+docker login --username=mcskyding@vip.qq.com registry.cn-hangzhou.aliyuncs.com
+
+docker network create --subnet=172.20.0.1/24 sky
+
+mkdir D:\Develop\php\etc
+copy php\7.4.8\etc\*.ini D:\Develop\php\etc
+
+
+mkdir D:\Develop\nginx\conf\cert
+copy nginx\1.19.1\conf\cert\*.* D:\Develop\nginx\conf\cert
+
+
+mkdir D:\Develop\nginx\conf\conf.d
+copy nginx\1.19.1\conf\conf.d\*.conf D:\Develop\nginx\conf\conf.d
+
+
+copy nginx\1.19.1\conf\nginx.conf D:\Develop\nginx\conf\nginx.conf
+
+docker-compose up -d
+
