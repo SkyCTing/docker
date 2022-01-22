e4f8a7ad6b3c38d9471113f096f6d0e646d41fe1
SkyTing <mcskyding@vip.qq.com>
2022-01-22 22:32:09 +0800

    增加docker默认的测试环境

--- first_run.bat
+++ first_run.bat
@@ -5,16 +5,14 @@ docker network create --subnet=172.20.0.1/24 sky
 mkdir D:\Develop\php\etc
 copy php\7.4.8\etc\*.ini D:\Develop\php\etc
 
-
 mkdir D:\Develop\nginx\conf\cert
 copy nginx\1.19.1\conf\cert\*.* D:\Develop\nginx\conf\cert
-
 
 mkdir D:\Develop\nginx\conf\conf.d
 copy nginx\1.19.1\conf\conf.d\*.conf D:\Develop\nginx\conf\conf.d
 
-
 copy nginx\1.19.1\conf\nginx.conf D:\Develop\nginx\conf\nginx.conf
 
+copy docker-compose.mac.yml docker-compose.yml
 docker-compose up -d
 
